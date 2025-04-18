19d1db36f605da74c701057ed552498280516a40
Ramon <ramonempresa2001@gmail.com>
2025-04-18 20:58:52 -0300

    ✨ feat: adiciona a captura do click no botão papel

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const rockbtn = document.getElementById('rock');
+const paperbtn = document.getElementById('paper');
 
 
 
 rockbtn.addEventListener('click', function(){
     playerChoice = 'pedra'
+})
+
+paperbtn.addEventListener('click', function(){
+    playerChoice = 'paper'
 })
