58ff9ad7e849727fd40442ec66eae685a06c2ddd
Ramon <ramonempresa2001@gmail.com>
2025-04-18 21:20:08 -0300

    ✨ feat: adiciona a função que faz o computador realizar uma escolha aleatória das opções

--- app.js
+++ app.js
@@ -14,3 +14,9 @@ paperbtn.addEventListener("click", function () {
 scissorsbtn.addEventListener("click", function () {
   playerChoice = "scissors";
 });
+
+function getComputerChoice(){
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomIndex = Math.floor(Math.random() * 3);
+    return choices[randomIndex];
+}
