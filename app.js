606053c0b01d30d84db64ed40ffcbe06eb6300ae
Ramon <ramonempresa2001@gmail.com>
2025-04-18 21:01:45 -0300

    ✨ feat: adiciona a captura do click no botão tesoura

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const rockbtn = document.getElementById('rock');
 const paperbtn = document.getElementById('paper');
+const scissorsbtn = document.getElementById('scissors');
 
 
 
 rockbtn.addEventListener('click', function(){
-    playerChoice = 'pedra'
+    playerChoice = 'rock'
 })
 
 paperbtn.addEventListener('click', function(){
     playerChoice = 'paper'
+})
+
+scissorsbtn.addEventListener('click', function(){
+    playerChoice = 'scissors'
 })
